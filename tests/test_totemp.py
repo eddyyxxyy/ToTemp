d326c41612bff9bd35b9df179bb613c24c4c47a3
Edson Pimenta <edson.tibo@gmail.com>
2022-11-14 14:57:45 -0300

    Adds Delisle to Newton tests

--- tests/test_totemp.py
+++ tests/test_totemp.py
@@ -325,3 +325,23 @@ class TestToTemp:
         with default parameter set to False
         """
         assert isinstance(Delisle.to_kelvin(99.227339, float_ret=False), int)
+
+    def test_delisle_to_newton(self) -> None:
+        """Tests the result of the conversion Delisle to Newton"""
+        assert Delisle.to_newton(1.98327392917266655) == 32.563679735582014
+
+    def test_delisle_to_newton_default_type(self) -> None:
+        """
+        Tests the type of the value returned on the conversion Delisle to Newton
+        with default parameter values
+        """
+        assert isinstance(Delisle.to_newton(1), float)
+
+    def test_delisle_to_newton_type_trunc_ret(self) -> None:
+        """
+        Tests the type of the value returned on the conversion Delisle to Newton
+        with default parameter set to False
+        """
+        assert isinstance(
+            Delisle.to_newton(1.98327392917266655, float_ret=False), int
+        )
