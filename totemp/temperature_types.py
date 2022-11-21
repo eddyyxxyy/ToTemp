f51f55bcd5fbe70888f57334515f914874d52132
lok0zzz <raul_bardela@hotmail.com>
2022-11-21 16:15:21 -0300

    Add Newton to Fahrenheit method

--- totemp/temperature_types.py
+++ totemp/temperature_types.py
@@ -488,3 +488,19 @@ class Newton:
         if float_ret:
             return float(newton / 0.33)
         return trunc(newton / 0.33)
+
+    @staticmethod
+    def to_fahrenheit(
+        newton: float | int, /, *, float_ret=True
+    ) -> float | int:
+        """
+        Converts Newton to Fahrenheit, returning a float by default.
+        If the float_ret parameter is False, it returns an approximate int value
+        (using the math's module trunc function).
+        :param Newton: Newton value to be converted
+        :param float_ret: Optional, True by default to return floats
+        :return: float or int
+        """
+        if float_ret:
+            return float((newton * 5.4545) + 32.00)
+        return trunc((newton * 5.4545) + 32.00)
