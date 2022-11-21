4c0d5a3e73538373e36359d50b732704480c6a0f
lok0zzz <raul_bardela@hotmail.com>
2022-11-21 15:27:06 -0300

    Update package version and mantainers

--- totemp/__init__.py
+++ totemp/__init__.py
@@ -6,10 +6,11 @@ from .temperature_types import Celsius, Delisle, Fahrenheit, Kelvin
 __author__ = 'Edson Pimenta and Dávilos Tavares'
 __credits__ = ['Edson Pimenta', 'Dávilos Tavares']
 __license__ = 'GPL-3.0'
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __maintainer__ = [
     'Edson Pimenta <edson.tibo@gmail.com>',
     'Dávilos Tavares <daviloscostagg@hotmail.com>',
+    'Raul Silva <raul_bardela@hotmail.com>',
 ]
 __status__ = 'Development'
 __all__ = ['Celsius', 'Fahrenheit', 'Delisle', 'Kelvin']
