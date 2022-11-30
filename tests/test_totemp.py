90dc801940ad4e4f65eb792b46b4e463d075e450
lok0zzz <raul_bardela@hotmail.com>
2022-11-30 15:21:28 -0300

    Updating Celsius to Rankine tests.

--- tests/test_totemp.py
+++ tests/test_totemp.py
@@ -110,19 +110,27 @@ class TestToTemp:
 
         assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
 
-    def test_celsius_to_rankine_default_type(self) -> None:
-        """
-        Tests the type of the value returned on the conversion Celsius to Rankine
-        with default parameter values
-        """
-        assert isinstance(Celsius.to_rankine(18), float)
+    def test_dynamic_type_return_celsius_to_rankine(self) -> None:
+        """Tests the dynamic typed results of the conversion Celsius to Rankine"""
+        temps = (
+            Celsius(randint(1, 20)).to_rankine(),
+            Celsius(uniform(0.0, 20.0)).to_rankine(),
+        )
+        errors = func_to_test_dynamic_returns(temps)
 
-    def test_celsius_to_rankine_type_trunc_ret(self) -> None:
-        """
-        Tests the type of the value returned on the conversion Celsius to Rankine
-        with default parameter set to False
-        """
-        assert isinstance(Celsius.to_rankine(18, float_ret=False), int)
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
+
+    def test_precise_rounded_celsius_to_rankine(self) -> None:
+        """Tests the rounded and precise result of the conversion Celsius to Rankine"""
+        temps = (
+            Celsius(25).precise().to_rankine(),
+            Rankine(value=536.6700000000001),
+            Celsius(25.25).rounded().to_rankine(),
+            Rankine(value=536),
+        )
+        errors = func_to_test_precise_rounded_results(temps)
+
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
 
     def test_celsius_to_reaumur(self) -> None:
         """Tests the result of the conversion Celsius to Réaumur"""
