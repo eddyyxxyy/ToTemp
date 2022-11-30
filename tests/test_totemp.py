793952f2f2c7177afd90e9b62ec15ec4728bdc31
Edson Pimenta <edson.tibo@gmail.com>
2022-11-30 11:53:41 -0300

    Updates Kelvin to Celsius tests

--- tests/test_totemp.py
+++ tests/test_totemp.py
@@ -284,23 +284,27 @@ class TestToTemp:
         )
 
     # Kelvin to <other temp scale> tests
-    def test_kelvin_to_celsius(self) -> None:
-        """Tests the result of the conversion Kelvin to Celsius"""
-        assert Kelvin.to_celsius(67.498259) == -205.65174099999996
+    def test_dynamic_type_return_kelvin_to_celsius(self) -> None:
+        """Tests the dynamic typed results of the conversion Kelvin to Celsius"""
+        temps = (
+            Kelvin(randint(1, 20)).to_celsius(),
+            Kelvin(uniform(0.0, 20.0)).to_celsius(),
+        )
+        errors = func_to_test_dynamic_returns(temps)
 
-    def test_kelvin_to_celsius_default_type(self) -> None:
-        """
-        Tests the type of the value returned on the conversion Kelvin to Celsius
-        with default parameter values
-        """
-        assert isinstance(Kelvin.to_celsius(10), float)
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
 
-    def test_kelvin_to_celsius_type_trunc_ret(self) -> None:
-        """
-        Tests the type of the value returned on the conversion Kelvin to Celsius
-        with default parameter set to False
-        """
-        assert isinstance(Kelvin.to_celsius(10.498259, float_ret=False), int)
+    def test_precise_rounded_kelvin_to_celsius(self) -> None:
+        """Tests the rounded and precise result of the conversion Kelvin to Celsius"""
+        temps = (
+            Kelvin(25).precise().to_celsius(),
+            Celsius(value=-248.14999999999998),
+            Kelvin(25.25).rounded().to_celsius(),
+            Celsius(value=-248),
+        )
+        errors = func_to_test_precise_rounded_results(temps)
+
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
 
     def test_kelvin_to_delisle(self) -> None:
         """Tests the result of the conversion Kelvin to Delisle"""
