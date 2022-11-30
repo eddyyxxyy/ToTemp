96edec67962b020be7480501827b849babb79c8e
lok0zzz <raul_bardela@hotmail.com>
2022-11-29 21:42:07 -0300

    Update Newton to Rankine tests

--- tests/test_totemp.py
+++ tests/test_totemp.py
@@ -610,6 +610,28 @@ class TestToTemp:
 
         assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
 
+    def test_dynamic_type_return_newton_to_rankine(self) -> None:
+        """Tests the dynamic typed results of the conversion Rankine to Kelvin"""
+        temps = (
+            Newton(randint(1, 20)).to_rankine(),
+            Newton(uniform(0.0, 20.0)).to_rankine(),
+        )
+        errors = func_to_test_dynamic_returns(temps)
+
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
+
+    def test_precise_rounded_newton_to_rankine(self) -> None:
+        """Tests the rounded and precise result of the conversion Newton to Rankine"""
+        temps = (
+            Newton(25).precise().to_rankine(),
+            Rankine(value=628.0325),
+            Newton(25.25).rounded().to_rankine(),
+            Rankine(value=628),
+        )
+        errors = func_to_test_precise_rounded_results(temps)
+
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
+
     def test_dynamic_type_return_newton_to_kelvin(self) -> None:
         """Tests the dynamic typed results of the conversion Newton to Kelvin"""
         temps = (
@@ -631,24 +653,6 @@ class TestToTemp:
         errors = func_to_test_precise_rounded_results(temps)
 
         assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
-
-    def test_newton_to_rankine(self) -> None:
-        """Tests the result of the conversion Newton to Rankine"""
-        assert Newton.to_rankine(3.331) == 509.83893950000004
-
-    def test_newton_to_rankine_default_type(self) -> None:
-        """
-        Tests the type of the value returned on the conversion Newton to Rankine
-        with default parameter values
-        """
-        assert isinstance(Newton.to_rankine(3), float)
-
-    def test_newton_to_rankine_type_trunc_ret(self) -> None:
-        """
-        Tests the type of the value returned on the conversion Newton to Rankine
-        with default parameter set to False
-        """
-        assert isinstance(Newton.to_rankine(3.331, float_ret=False), int)
 
     def test_newton_to_romer(self) -> None:
         """Tests the result of the conversion Newton to Rømer"""
