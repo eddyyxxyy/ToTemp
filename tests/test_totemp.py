d9339365715658bd738272d88fee99adb05f665a
lok0zzz <raul_bardela@hotmail.com>
2022-11-29 21:15:21 -0300

    Update Newton to Fahrenheit tests

--- tests/test_totemp.py
+++ tests/test_totemp.py
@@ -566,23 +566,27 @@ class TestToTemp:
 
         assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
 
-    def test_newton_to_fahrenheit(self) -> None:
-        """Tests the result of the conversion Newton to Fahrenheit"""
-        assert Newton.to_fahrenheit(3.331) == 50.16909090909091
+    def test_dynamic_type_return_newton_to_fahrenheit(self) -> None:
+        """Tests the dynamic typed results of the conversion Newton to Fahrenheit"""
+        temps = (
+            Newton(randint(1, 20)).to_fahrenheit(),
+            Newton(uniform(0.0, 20.0)).to_fahrenheit(),
+        )
+        errors = func_to_test_dynamic_returns(temps)
 
-    def test_newton_to_fahreinheit_default_type(self) -> None:
-        """
-        Tests the type of the value returned on the conversion Newton to Fahrenheit
-        with default parameter values
-        """
-        assert isinstance(Newton.to_fahrenheit(3), float)
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
 
-    def test_newton_to_fahreinheit_type_trunc_ret(self) -> None:
-        """
-        Tests the type of the value returned on the conversion Newton to Fahrenheit
-        with default parameter set to False
-        """
-        assert isinstance(Newton.to_fahrenheit(3.331, float_ret=False), int)
+    def test_precise_rounded_newton_to_fahrenheit(self) -> None:
+        """Tests the rounded and precise result of the conversion Newton to Fahrenheit"""
+        temps = (
+            Newton(25).precise().to_fahrenheit(),
+            Fahrenheit(value=168.36363636363637),
+            Newton(25.25).rounded().to_fahrenheit(),
+            Fahrenheit(value=168),
+        )
+        errors = func_to_test_precise_rounded_results(temps)
+
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
 
     def test_newton_to_delisle(self) -> None:
         """Tests the result of the conversion Newton to Delisle"""
