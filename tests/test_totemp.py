f4076a6261087377fa38bcac98fa12f06528f078
Edson Pimenta <edson.tibo@gmail.com>
2022-12-01 10:34:27 -0300

    Updates Delisle to Fahrenheit tests

--- tests/test_totemp.py
+++ tests/test_totemp.py
@@ -489,25 +489,27 @@ class TestToTemp:
 
         assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
 
-    def test_delisle_to_fahrenheit(self) -> None:
-        """Tests the result of the conversion Delisle to Fahrenheit"""
-        assert Delisle.to_fahrenheit(10.28723) == 199.655324
-
-    def test_delisle_to_fahrenheit_default_type(self) -> None:
-        """
-        Tests the type of the value returned on the conversion Delisle to Fahrenheit
-        with default parameter values
-        """
-        assert isinstance(Delisle.to_fahrenheit(10), float)
-
-    def test_delisle_to_fahrenheit_type_trunc_ret(self) -> None:
-        """
-        Tests the type of the value returned on the conversion Delisle to Fahrenheit
-        with default parameter set to False
-        """
-        assert isinstance(
-            Delisle.to_fahrenheit(10.28723, float_ret=False), int
+    def test_dynamic_type_return_delisle_to_fahrenheit(self) -> None:
+        """Tests the dynamic typed results of the conversion Delisle to Fahrenheit"""
+        temps = (
+            Delisle(randint(1, 20)).to_fahrenheit(),
+            Delisle(uniform(0.0, 20.0)).to_fahrenheit(),
         )
+        errors = func_to_test_dynamic_returns(temps)
+
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
+
+    def test_precise_rounded_delisle_to_fahrenheit(self) -> None:
+        """Tests the rounded and precise result of the conversion Delisle to Fahrenheit"""
+        temps = (
+            Delisle(25).precise().to_fahrenheit(),
+            Fahrenheit(value=182.0),
+            Delisle(25.25).rounded().to_fahrenheit(),
+            Fahrenheit(value=182),
+        )
+        errors = func_to_test_precise_rounded_results(temps)
+
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
 
     def test_delisle_to_kelvin(self) -> None:
         """Tests the result of the conversion Delisle to Kelvin"""
