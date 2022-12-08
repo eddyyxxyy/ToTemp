f2571616a7efba0ef48f472a2571bb64e4db2d9a
Edson Pimenta <edson.tibo@gmail.com>
2022-12-08 09:34:09 -0300

    Tests: adds tests to Réaumur to Celsius method

--- tests/test_totemp.py
+++ tests/test_totemp.py
@@ -951,6 +951,29 @@ class TestToTemp:
 
         assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
 
+    # Réaumur to <other temp scale> tests
+    def test_dynamic_type_return_reaumur_to_celsius(self) -> None:
+        """Tests the dynamic typed results of the conversion Réaumur to Celsius"""
+        temps = (
+            Reaumur(randint(1, 20)).to_celsius(),
+            Reaumur(uniform(0.0, 20.0)).to_celsius(),
+        )
+        errors = func_to_test_dynamic_returns(temps)
+
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
+
+    def test_precise_rounded_reaumur_to_celsius(self) -> None:
+        """Tests the rounded and precise result of the conversion Réaumur to Celsius"""
+        temps = (
+            Reaumur(25).precise().to_celsius(),
+            Celsius(value=31.25),
+            Reaumur(25.25).rounded().to_celsius(),
+            Celsius(value=31),
+        )
+        errors = func_to_test_precise_rounded_results(temps)
+
+        assert not errors, 'errors occurred:\n{}'.format('\n'.join(errors))
+
     # Rømer to <other temp scale> tests
     def test_dynamic_type_return_romer_to_celsius(self) -> None:
         """Tests the dynamic typed results of the conversion Rømer to Celsius"""
