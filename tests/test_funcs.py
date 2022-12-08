fcc379c7c3d900152831763f5fb7881bfa562c09
Edson Pimenta <edson.tibo@gmail.com>
2022-12-07 21:54:06 -0300

    Refactor: now the test checks if temps[2].value is and int instance

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -20,5 +20,7 @@ def func_to_test_precise_rounded_results(temps: tuple) -> list:
         errors.append(f'{temps[0]} != ({temps[1]}) -> should be equal')
     if not temps[2] == temps[3]:
         errors.append(f'{temps[2]} != ({temps[3]}) -> should be equal')
+    if not isinstance(temps[2].value, int):
+        errors.append(f'({temps[2]}) -> should be int')
 
     return errors
