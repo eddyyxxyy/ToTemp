6cbfec07c885c6baf0a0361cfc701e629a9cc73c
Edson Pimenta <edson.tibo@gmail.com>
2023-02-06 17:20:04 -0300

    Docs: changed path to module again

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../totemp'))
+sys.path.insert(0, os.path.abspath('../../totemp'))
 
 
 # -- Project information -----------------------------------------------------
